fb7d44b86accc421c5b0e95e561f029e290e5275
Oluwanishola Ayeotan <143749368+Shola-Ayeotan@users.noreply.github.com>
2025-01-04 22:45:15 +0000

    Update InferenceRAG.py

--- RAGToSQL/InferenceRAG.py
+++ RAGToSQL/InferenceRAG.py
@@ -3,7 +3,7 @@ import os
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-vn = MyVanna(config={'api_key': 'sk-proj-dCuD8lty0RfhpudPewkIT3BlbkFJc5TxBDMPyApHTzkCPdyv', 'model': 'gpt-3.5-turbo-16k'})
+vn = MyVanna(config={'api_key': '', 'model': 'gpt-3.5-turbo-16k'})
 
 # print(vn.get_training_data())
 
